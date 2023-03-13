903711975c649ef58cf4c97537e81cc914691f18
Jonas Bernoulli <jonas@bernoul.li>
2023-03-13 19:41:05 +0100

    Update homepage link
    Also don't claim Yanghao Xie maintains this fork.

--- which-key-posframe.el
+++ which-key-posframe.el
@@ -2,9 +2,8 @@
 
 ;; Copyright (C) 2019 Yanghao Xie
 
-;; Author: Yanghao Xie
-;; Maintainer: Yanghao Xie <yhaoxie@gmail.com>
-;; URL: https://github.com/yanghaoxie/which-key-posframe
+;; Author: Yanghao Xie <yhaoxie@gmail.com>
+;; Homepage: https://github.com/emacsorphanage/which-key-posframe
 ;; Version: 0.2.0
 ;; Keywords: convenience, bindings, tooltip
 ;; Package-Requires: ((emacs "26.0") (posframe "0.4.3") (which-key "3.3.2"))
